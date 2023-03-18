8d291fd9a219a6d173533d803194d8de22c1e8b5
kas101 <kasjacob@outlook.com>
2023-03-18 18:03:05 +0200

    Regiter UI Screen done

--- components/Register.jsx
+++ components/Register.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Button, Image, StyleSheet, Text, View } from 'react-native'
-import { TextInput } from 'react-native-gesture-handler'
+import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native'
+
 
 //Regiter screen
 //administor user registration
